93189b46f125b5107370527dfb308215977a3806
djala25000-cmyk <djala25000@gmail.com>
2026-06-08 20:19:14 +0200

    Update chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -13,7 +13,7 @@ exports.handler = async function(event, context) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        model: 'llama3-8b-8192',
+        model: 'llama-3.3-70b-versatile',
         messages: [
           {
             role: 'system',
